ebcfe01447a34609e784e339d0167ee0e7cbb880
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-03-21 17:46:39 +0900

    push

---
+++ Machine Learning/train_test_split.py
@@ -0,0 +1,24 @@
+import collections
+from sklearn.model_selection import train_test_split
+
+TRAIN_SIZE = 0.7
+VAL_SIZE = 0.15
+TEST_SIZE = 0.15
+
+def train_val_test_split(X, y, train_size):
+    """Split dataset into data splits."""
+    X_train, X_, y_train, y_ = train_test_split(X, y, train_size=TRAIN_SIZE, stratify=y)
+    X_val, X_test, y_val, y_test = train_test_split(X_, y_, train_size=0.5, stratify=y_)
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+    # Data
+X = preprocessed_df["title"].values
+y = preprocessed_df["category"].values
+
+# Create data splits
+X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(
+    X=X, y=y, train_size=TRAIN_SIZE)
+print (f"X_train: {X_train.shape}, y_train: {y_train.shape}")
+print (f"X_val: {X_val.shape}, y_val: {y_val.shape}")
+print (f"X_test: {X_test.shape}, y_test: {y_test.shape}")
+print (f"Sample point: {X_train[0]} → {y_train[0]}")
