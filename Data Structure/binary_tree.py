883949d33fa848132d877e20c7f4cf2b47c3c274
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-16 13:34:01 +0900

    change

--- Data Structure/binary_tree.py
+++ Data Structure/binary_tree.py
@@ -19,3 +19,28 @@ class BinaryTree:
             new_node = BinaryTree(value)
             new_node.right_child = self.right_child
             self.right_child = new_node
+
+
+
+# Example Tree: https://cdn-media-1.freecodecamp.org/images/1*V_EUgNXVc8Wy9H1-JoqT3g.jpeg            
+a_node = BinaryTree('a')
+a_node.insert_left('b')
+a_node.insert_right('c')
+
+b_node = a_node.left_child
+b_node.insert_right('d')
+
+c_node = a_node.right_child
+c_node.insert_left('e')
+c_node.insert_right('f')
+
+d_node = b_node.right_child
+e_node = c_node.left_child
+f_node = c_node.right_child
+
+print(a_node.value) # a
+print(b_node.value) # b
+print(c_node.value) # c
+print(d_node.value) # d
+print(e_node.value) # e
+print(f_node.value) # f
