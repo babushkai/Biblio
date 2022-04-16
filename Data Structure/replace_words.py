f3126d97d9c6ca6930fc4d9c358e76dbae186949
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-16 14:39:56 +0900

    change

--- Data Structure/replace_words.py
+++ Data Structure/replace_words.py
@@ -1,20 +1,22 @@
-    def replaceWords(self, roots, sentence):
-        _trie = lambda: collections.defaultdict(_trie)
-        trie = _trie()
-        END = True
-        for root in roots:
-            cur = trie
-            for letter in root:
-                cur = cur[letter]
-            cur[END] = root
+# https://leetcode.com/problems/replace-words
 
-        def replace(word):
-            cur = trie
-            for letter in word:
-                if letter not in cur: break
-                cur = cur[letter]
-                if END in cur:
-                    return cur[END]
-            return word
+def replaceWords(self, roots, sentence):
+    _trie = lambda: collections.defaultdict(_trie)
+    trie = _trie()
+    END = True
+    for root in roots:
+        cur = trie
+        for letter in root:
+            cur = cur[letter]
+        cur[END] = root
 
-        return " ".join(map(replace, sentence.split()))
+    def replace(word):
+        cur = trie
+        for letter in word:
+            if letter not in cur: break
+            cur = cur[letter]
+            if END in cur:
+                return cur[END]
+        return word
+
+    return " ".join(map(replace, sentence.split()))
