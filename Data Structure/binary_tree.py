4749cbcae80b170c51a06cf623c38997dca8c0a8
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-16 14:15:47 +0900

    change

--- Data Structure/binary_tree.py
+++ Data Structure/binary_tree.py
@@ -113,15 +113,13 @@ class BinarySearchTree:
         self.left_child = None
         self.right_child = None
 
-    def insert_node(self, value):
-        if value <= self.value and self.left_child:
-            self.left_child.insert_node(value)
-        elif value <= self.value:
-            self.left_child = BinarySearchTree(value)
-        elif value > self.value and self.right_child:
-            self.right_child.insert_node(value)
-        else:
-            self.right_child = BinarySearchTree(value)
+    def find_node(self, value):
+        if value < self.value and self.left_child:
+            return self.left_child.find_node(value)
+        if value > self.value and self.right_child:
+            return self.right_child.find_node(value)
+
+        return value == self.value
 
 
 
