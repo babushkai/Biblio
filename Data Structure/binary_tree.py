84f98bb71d4e5f440b5501ee424b87556a3d606f
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-16 13:24:30 +0900

    change

--- Data Structure/binary_tree.py
+++ Data Structure/binary_tree.py
@@ -5,17 +5,17 @@ class BinaryTree:
         self.right_child = None # No node
 
     def insert_left(self, value):
-    if self.left_child == None:
-        self.left_child = BinaryTree(value)
-    else:
-        new_node = BinaryTree(value)
-        new_node.left_child = self.left_child
-        self.left_child = new_node
+        if self.left_child == None:
+            self.left_child = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node.left_child = self.left_child
+            self.left_child = new_node
 
     def insert_right(self, value):
-    if self.right_child == None:
-        self.right_child = BinaryTree(value)
-    else:
-        new_node = BinaryTree(value)
-        new_node.right_child = self.right_child
-        self.right_child = new_node
+        if self.right_child == None:
+            self.right_child = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node.right_child = self.right_child
+            self.right_child = new_node
