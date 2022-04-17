5f0a7a97875cea4cb2c512828eb1d798d0ea3436
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-18 06:28:51 +0900

    change

--- Data Structure/phonebook.py
+++ Data Structure/phonebook.py
@@ -13,3 +13,26 @@ def phonebook():
                 print('Not found')
         except:
             break
+
+# Decorated
+def phonebook():
+    print("Hello!, how many people would you like to register?")
+    num_of_register = int(input())
+    print(f"Proceed with {num_of_register} people")
+    name_number = [input().split() for i in range(num_of_register)]
+    phone_book = {k:v for k, v in name_number}
+    while True:
+        try:
+            print("Retrieve your name here")
+            name = input()
+            if name in phone_book:
+                print("Your registration number is: ")
+                print( phone_book[name] )
+                print("Would you like to continue?")
+                answer = input()
+                if answer == "yes":
+                    continue
+                else:
+                    break
+        except:
+            break
