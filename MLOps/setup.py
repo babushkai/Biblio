4cb2849dd2e0df0b0ec79d4e0e0f7e08b4f8b786
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-21 07:11:26 +0900

    chage

---
+++ MLOps/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+test =["pandas" ]
+
+setup(name="dk", 
+    license = "MIT", 
+    description = "test", 
+    author = "Daisuke Kuwabara", 
+    install_requires = test)
