59ee84babcfbc4f6ce15063c115eb7b87c8cb512
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-23 09:51:48 +0900

    change

--- MLOps/Babushka/setup.py
+++ MLOps/Babushka/setup.py
@@ -16,7 +16,7 @@ setup(name="dk",
     install_requires = test,
     entry_points = {
         "console_scripts": [
-            "babushuka" = "babushuka.main:app",
+            "babushuka" = "babushka.main:app",
         ],
     },
     )
