ff3cb4fec12fe8a4dbcc0d8f8376f799d997b646
Daisuke Kuwabara <daisuke0582@gmail.com>
2022-04-30 11:28:50 +0900

    change

--- SoftwareEngineering/SOLID/dependency_inversion.py
+++ SoftwareEngineering/SOLID/dependency_inversion.py
@@ -8,3 +8,38 @@ There comes a point in software development where our app will be largely compos
 When this happens, we have to clear things up by using dependency injection. 
 High-level components depending on low-level components to function.
 """
+
+
+class Database:
+    def __init__(self, name):
+        self.name = name
+        
+        
+    def connection_name(self):
+        print(self.name)
+    
+    def get_table(self):
+        fetch = func(self.name)
+        return fetch
+
+    def get_structure(self):
+        pass
+        
+    def get_view(self, table, query):
+        table = get_table()
+        view = query(table)
+
+# Inheritance: is-a-relationship
+class Structured(Database):
+    def __init__(self):
+        pass
+
+# Aggregation: has-a-relationship
+class Unstructured:
+    def __init__(self, database: Database):
+        self.database = database
+    
+
+"""
+High-level modules should not depend on low-level level modules. It should depend upon its abstraction.
+"""
