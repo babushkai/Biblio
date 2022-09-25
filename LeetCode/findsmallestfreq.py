a6fa70096a4551d4da19f182d442b4cdfe258f0d
babushka <daisuke0582@gmail.com>
2022-09-26 08:47:00 +0900

    change

--- LeetCode/findsmallestfreq.py
+++ LeetCode/findsmallestfreq.py
@@ -1,5 +1,6 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        from collections import Counter
-        frequency = Counter(nums)
-        return frequency.most_common()[-1][0]
+from typing import List
+
+def singleNumber(self, nums: List[int]) -> int:
+    from collections import Counter
+    frequency = Counter(nums)
+    return frequency.most_common()[-1][0]
