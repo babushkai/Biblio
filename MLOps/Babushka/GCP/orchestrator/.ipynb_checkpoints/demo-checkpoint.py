740ce9ed2137556fd83e099a7966417246fd145b
kwdaisuke <daisuke0582@gmail.com>
2022-10-08 16:40:35 +0900

    adding file

---
+++ MLOps/Babushka/GCP/orchestrator/.ipynb_checkpoints/demo-checkpoint.py
@@ -0,0 +1,21 @@
+test = [2,4,1,2,0,2,4,0,2,3,0,2,0,0,0]
+
+for i in range(len(test)-1):
+    for j in range(len(test)-1):
+        if test[j] == 0:
+            test[j], test[j + 1] = test[j+1], test[j]
+            
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    
+    gcd(b, a%b)
+    
+
+a, b = (4, 20), (20, 4), (0, 4)
+    
+
+
+
+
---
+++ MLOps/Babushka/GCP/orchestrator/dag1.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from great_expectations_provider.operators.great_expectations import (
+    GreatExpectationsOperator,
+)
+
+from airflow.decorators import dag
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.utils.dates import days_ago
+from app import cli, 
+
+
+a = str(123)
