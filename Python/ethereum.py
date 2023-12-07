ea5ce67f80da2324e3c48d879a006fef446f2aa1
kwdaisuke <daisuke0582@gmail.com>
2023-12-08 00:32:45 +0900

    change

--- Python/ethereum.py
+++ Python/ethereum.py
@@ -32,4 +32,4 @@ eth_price = data["data"][0]["quote"]["USD"]["price"]
 # Print the price
 print(f"The current price of Ethereum is ${eth_price:.2f}.")
 
-"This program uses the API endpoint and parameters to retrieve the latest listing information for Ethereum, and then parses the response to extract the current price in US dollars. The program then prints the price to the console. This is just an example, and there are many other ways to write a Python program to retrieve the price of Ethereum using the CoinMarketCap API or other data sources."
+# "This program uses the API endpoint and parameters to retrieve the latest listing information for Ethereum, and then parses the response to extract the current price in US dollars. The program then prints the price to the console. This is just an example, and there are many other ways to write a Python program to retrieve the price of Ethereum using the CoinMarketCap API or other data sources."
