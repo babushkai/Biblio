f3ebe9813a61a5a132766448947870bfe0c1fc6c
Daisuke Kuwabara <daisuke0582@gmail.com>
2024-03-06 08:55:17 +0900

    Update two_sum.py

--- Data Structure/two_sum.py
+++ Data Structure/two_sum.py
@@ -15,3 +15,14 @@ def twoSum(nums, target):
                 return [i, j]
             else:
                 continue
+
+def twoSum(nums, target):
+    # Dictionary to keep track of numbers and their indices
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+    return []
+
